ce21ceab63a797080d8406028f4592883efa03f4
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-02-25 04:44:11 +0530

    [Ajay]-Welcome to linked list-

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to data structure programs");
+        }
+    }
+}
