512a73a81877861773335ad544ae3e77595ac772
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-02-25 04:52:50 +0530

    [Ajay]-Create linked list-

--- Program.cs
+++ Program.cs
@@ -5,6 +5,24 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to data structure programs");
+            LinkedList linkedlist = new LinkedList();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Please enter an option");
+                Console.WriteLine("1::Create linked list ");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        linkedlist.Add(56);
+                        linkedlist.Add(30);
+                        linkedlist.Add(70);
+                        break;
+                }
+            }
         }
     }
 }
+    
